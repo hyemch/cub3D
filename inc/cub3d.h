63e47cf067639e8c6791ab546ca69c3ff0497e18
hyemch <blossoming33@gmail.com>
2023-05-14 22:20:36 +0900

    mlx

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -13,6 +13,7 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../mlx/mlx.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
--- src/main.c
+++ src/main.c
@@ -12,7 +12,32 @@
 
 #include "../inc/cub3d.h"
 
-int	main(int argc, char *argv)
+int	map_extension(char *filename)
 {
+	int	i;
+
+	i = 0;
+	while (filename && filename[i] != '\0')
+		i++;
+	if (filename[i - 4] != '.' || filename[i - 3] != 'c' || \
+		filename[i - 2] != 'u' || filename[i - 1] != 'b')
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	void	*mlx;
+	void	*win;
+
+	(void)argv;
+	if (argc != 2)
+		return (write(2, "ERROR\nArgument count error\n", 27));
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 200, 200, "cub3D");
+//	mlx_loop_hook(game.mlx, draw_map, &game);
+//	mlx_hook(game.win, X_EVENT_KEY_RELEASE, 0, key_hook, &game);
+//	mlx_hook(game.win, X_EVENT_KEY_EXIT, 0, exit_game, &game);
+//	mlx_loop(game.mlx);
 	return (0);
 }
